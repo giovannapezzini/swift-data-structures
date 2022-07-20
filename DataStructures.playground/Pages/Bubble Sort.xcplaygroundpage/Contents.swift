571840a623614aaa58457dc7fa7928e71fe36de4
giovannapezzini <giovannarpezzini@gmail.com>
2022-07-20 11:48:27 +0900

    add bubble sort content

---
+++ DataStructures.playground/Pages/Bubble Sort.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+/*: Bubble Sort Introduction
+ # Bubble Sort
+ ### Compares & swaps adjacent values 🫧
+ 
+ Iterates through a collection, compares adjacent values and swaps then into a sorted order.
+ 
+ The larger values in a set "bubble up" to the end of the collection.
+ 
+ 🥶 One of the worst performing sorts (unsorted collections)
+ 
+ * note:
+Best case scenario O(n)\
+Worst case scenario O(n²)
+ */
