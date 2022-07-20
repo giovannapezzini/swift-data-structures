ec9bee5e59ab6701a3b6797eef5ace20a5d44dd4
giovannapezzini <giovannarpezzini@gmail.com>
2022-07-20 12:25:06 +0900

    create selection sort content

--- DataStructures.playground/Pages/Selection Sort.xcplaygroundpage/Contents.swift
+++ DataStructures.playground/Pages/Selection Sort.xcplaygroundpage/Contents.swift
@@ -1,7 +1,15 @@
-//: [Previous](@previous)
-
-import Foundation
-
-var greeting = "Hello, playground"
-
-//: [Next](@next)
+/*: Selection Sort Introduction
+ # Selection Sort
+ ### Only swaps at the end of each pass. ☑️
+ 
+ Similar to Bubble Sort.
+ 
+ Pulls the lower values to the bottom. It will find the lowest unsorted value and swap it into place.
+ 
+ ### Example
+ 
+ 9 → 4 → 10 → 3    *find the lowest value*\
+ 3 → 4 → 10 → 9    *swaps 3 with the first value*\
+ 9 → 4 → 9 → 10    *4 is already in place, 9 is the next lowest number*
+ 
+*/
