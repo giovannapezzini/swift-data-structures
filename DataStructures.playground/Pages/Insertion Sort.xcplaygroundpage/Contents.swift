601f6f9b0063968ff8238bfb196e20a8955d56e7
giovannapezzini <giovannarpezzini@gmail.com>
2022-07-20 12:24:52 +0900

    create insertion sort content

--- DataStructures.playground/Pages/Insertion Sort.xcplaygroundpage/Contents.swift
+++ DataStructures.playground/Pages/Insertion Sort.xcplaygroundpage/Contents.swift
@@ -1,7 +1,18 @@
-//: [Previous](@previous)
+/*: Insertion Sort Introduction
+ # Insertion Sort
+ ### Slightly more performant than Bubble and Selection Sort. 🔙
 
-import Foundation
+ Iterates only once through the entire collection, from left to right.
+ 
+ * note:
+ Average time complexity of O(n²)\
+Best-case scenario, the data is already sorted: O(n)
+ 
+ ### Example
+ 
+ 9 → 4 → 10 → 3\
+ 4 → 9 → 10 → 3    *swaps 4 and 9*\
+ 3 → 4 → 9 → 10    *3 shifts all the way to the left*
+ 
+*/
 
-var greeting = "Hello, playground"
-
-//: [Next](@next)
