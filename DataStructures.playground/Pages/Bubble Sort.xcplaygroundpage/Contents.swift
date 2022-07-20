5b52c2cd27f37635efd22b8fd62a61d15cbbb54e
giovannapezzini <giovannarpezzini@gmail.com>
2022-07-20 12:24:28 +0900

    add example of bubble sort

--- DataStructures.playground/Pages/Bubble Sort.xcplaygroundpage/Contents.swift
+++ DataStructures.playground/Pages/Bubble Sort.xcplaygroundpage/Contents.swift
@@ -11,4 +11,12 @@
  * note:
 Best case scenario O(n)\
 Worst case scenario O(n²)
+ 
+ ### Example
+ 
+ 9 → 4 → 10 → 3\
+ 4 → 9 → 3 → 10   *first pass*\
+ 4 → 3 → 9 → 10   *second pass*\
+ 3 → 4 → 9 → 10   *last pass, no need to swap any values*
+
  */
