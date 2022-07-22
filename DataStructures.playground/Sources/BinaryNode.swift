fd149391b6553102e4c7a329b34635483f015572
giovannapezzini <giovannarpezzini@gmail.com>
2022-07-22 16:41:19 +0900

    add CustomStringConvertible extension to customize description

--- DataStructures.playground/Sources/BinaryNode.swift
+++ DataStructures.playground/Sources/BinaryNode.swift
@@ -9,3 +9,27 @@ public class BinaryNode<T> {
         self.value = value
     }
 }
+
+extension BinaryNode: CustomStringConvertible {
+  //Note: This algorithm is based on an implementation by Károly Lőrentey in his book Optimizing Collections, available from https://www.objc.io/books/optimizing-collections/.
+  
+  public var description: String { return diagram(for: self) }
+  
+  private func diagram(for node: BinaryNode?,
+                       _ top: String = "",
+                       _ root: String = "",
+                       _ bottom: String = "") -> String {
+      
+    guard let node = node else { return root + "nil\n" }
+      
+    if node.leftChild == nil && node.rightChild == nil {
+      return root + "\(node.value)\n"
+    }
+      
+    return diagram(for: node.rightChild,
+                   top + " ", top + "┌── ", top + "│ ")
+      + root + "\(node.value)\n"
+      + diagram(for: node.leftChild,
+                bottom + "│ ", bottom + "└── ", bottom + " ")
+  }
+}
